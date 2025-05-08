275243bcbe67142320d2edf3ff62e9d2df540a4e
Miguel Vasconcelos <vasconmiguel03@gmail.com>
2025-05-08 15:40:53 -0400

    initial commit: added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font_size = 12.0
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = 'Catppuccin Mocha'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
+config.window_padding = {
+  left = 20,
+  right = 20,
+  top = 20,
+  bottom = 20,
+}
+
+return config
